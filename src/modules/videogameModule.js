0038235daf7a6453fea90241829e423da59c2ddb
Alejandro Centeno <alejocm21@gmail.com>
2023-01-20 21:49:53 -0300

    Add format to response all videogames and  detail videogame

--- src/modules/videogameModule.js
+++ src/modules/videogameModule.js
@@ -2,8 +2,17 @@ const { videogameService, detailGameService } = require("../services/videogameSe
 
 const videogameModule = async () => {
   try {
-    const videogames = await videogameService()
-    return videogames;
+    const videogames = await videogameService();
+    const allVideogames = videogames.results.map(v => {
+      return {
+        id: v.id,
+        name: v.name,
+        image: v.background_image,
+        rating: v.rating,
+        released: v.released
+      };
+    });
+    return allVideogames;
   } catch (error) {
     throw error;
   }
@@ -11,8 +20,39 @@ const videogameModule = async () => {
 
 const detailGameModule = async (id) => {
   try {
-    const detailGame = await detailGameService(id)
-    return detailGame;
+    const detailGame = await detailGameService(id);
+
+    const platforms = detailGame.platforms.map(p => {
+      return {
+        id: p.platform.id,
+        name: p.platform.name,
+        image: p.platform.image_background,
+        requirements: {
+          minimum: p.requirements.minimum || "",
+          recommended: p.requirements.recommended || "",
+        }
+      }
+    })
+
+    const ratings = detailGame.ratings.map(r => {
+      return {
+        title: r.title,
+        percent: r.percent,
+        count: r.count,
+      }
+    })
+
+    const detailGameFormatted = {
+      id: detailGame.id,
+      name: detailGame.name,
+      description: detailGame.description,
+      image: detailGame.background_image,
+      platforms: platforms,
+      released: detailGame.released,
+      rating: detailGame.rating,
+      ratings: ratings,
+    }
+    return detailGameFormatted;
   } catch (error) {
     throw error;
   }
